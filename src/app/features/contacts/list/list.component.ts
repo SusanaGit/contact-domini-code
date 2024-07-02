243bbb0b73237670cdd69208a0506683d28da3a2
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-07-02 12:42:55 +0200

    10. create component features/contacts/list

---
+++ src/app/features/contacts/list/list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './list.component.html',
+  styles: ``
+})
+export class ListComponent {
+
+}
