d2b24eebc6cf2a2c2fd493b9a8ff1b592eb72285
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-07-02 12:26:56 +0200

    09. add angular/fire and configure app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import {environment} from "../environments/environment";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())]
 };
