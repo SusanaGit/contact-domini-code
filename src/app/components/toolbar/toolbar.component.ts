0cc458c6830717ecdf603ffc515f08fd856b5951
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-07-05 14:16:39 +0200

    23. add the <mat-toolbar> component to the ToolbarComponent structure

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 
 const MATERIAL_MODULES = [
   MatToolbarModule,
@@ -10,11 +12,22 @@ const MATERIAL_MODULES = [
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MATERIAL_MODULES],
+  imports: [MATERIAL_MODULES, MatButtonModule, RouterLink],
   template: `
-    <p>
-      toolbar works!
-    </p>
+    <mat-toolbar color="primary">
+      <a mat-button routerLink="/">
+        <mat-icon>home</mat-icon>
+        <span>Home</span>
+      </a>
+      <a mat-button routerLink="/contacts">
+        <mat-icon>list_alt</mat-icon>
+        <span>Contacts</span>
+      </a>
+      <a mat-button >
+        <mat-icon>add_box</mat-icon>
+        <span>New</span>
+      </a>
+    </mat-toolbar>
   `,
   styles: ``
 })
