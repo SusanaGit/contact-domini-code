d5e1941515832febbbbfab0e1cb7dc8004537914
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-07-05 11:27:56 +0200

    19. define the wildcard so that any URL that does not match the defined routes redirects to ‘/contacts’

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
 
   { path: '', redirectTo: '/contacts', pathMatch: 'full' },
-  { path: 'contacts', loadChildren: () => import('./features/contacts/contacts.routes')}
+  { path: 'contacts', loadChildren: () => import('./features/contacts/contacts.routes') },
+  { path: '**', redirectTo: '/contacts' }
 
 ];
